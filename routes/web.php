7e9756e6b70fdd71a7276aa6c772b78e98bb3066
lucas-0331 <lucas6.silva@alunos.ifsuldeminas.edu.br>
2024-01-09 13:01:14 -0300

    chore: de listagem e edição das tarefas acrescentadas

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,15 @@ use App\Http\Controllers\SiteController;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-
 //Route::resource('admin', TaskController::class)->except(['destroy', 'edit'])->names('task');
 Route::resource('admin', TaskController::class)->names('admin');
 
 Route::get('/', [SiteController::class, 'index'])->name('index');
 
 Route::get('/task/{id}', [SiteController::class, 'details'])->name('details');
+
+Route::get('/task/all/{id_user}', [SiteController::class, 'id_user'])->name('id_user');
+
+Route::get('/task/edit/{id}', [SiteController::class, 'edit'])->name('edit');
+
+Route::post('/task/edited', [SiteController::class, 'edited'])->name('edited');
