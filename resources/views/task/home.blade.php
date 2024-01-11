4a9522bccb49625f763dcd51dfdfeee348bc10f8
lucas-0331 <lucas6.silva@alunos.ifsuldeminas.edu.br>
2024-01-10 23:43:17 -0300

    chore: implementado a condicional de retorno de sucess e fail na view home

--- resources/views/task/home.blade.php
+++ resources/views/task/home.blade.php
@@ -2,8 +2,21 @@
 
 @section('title', 'Home')
 
+@push('jquery')
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+@endpush
+
 @section('content')
     <div class="relative overflow-x-auto shadow-md ">
+        @if($message = Session::get('success'))
+            <div class="flex justify-center bg-green-700 items-center text-3xl font-bold text-white h-20">
+                <span id="success_message" class="font-bold">{{ $message }}</span>
+            </div>
+        @elseif($message = Session::get('fail'))
+            <div class="flex justify-center bg-red-600 items-center text-4xl font-bold text-white h-20">
+                <span id="fail_message" class="font-bold">{{ $message }}</span>
+            </div>
+        @endif
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             @if(isset($tasks))
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -34,7 +47,7 @@
                         <td class="w-4 p-4">
                             <div class="flex items-center p-2">
                                 <input id="checkbox-table-search-{{ $task->id }}" type="checkbox" @if($task->status) checked @endif class="w-7 h-7 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                <label for="checkbox-table-search-1" class="sr-only">{{ $task->status }}</label>
+                                <label for="checkbox-table-search-{{ $task->id }}" class="sr-only">{{ $task->status }}</label>
                             </div>
                         </td>
                         <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
@@ -80,7 +93,7 @@
                                     edit_square
                                 </span>
                             </a>
-                            <a href="#" class="font-medium text-red-600 dark:text-red-500 hover:underline ms-3">
+                            <a href="#" onclick="confirm_delete('{{ route('delete_task', $task->id) }}', '{{ $task->name }}')" class="font-medium text-red-600 dark:text-red-500 hover:underline ms-3">
                                 <span class="material-symbols-outlined">
                                     delete
                                 </span>
@@ -93,6 +106,16 @@
                 <h3>Não existem tarefas!</h3>
            @endif
         </table>
+    </div>
+
+    <div id="confirm_delete_modal" class="modal z-50 rounded hidden fixed top-1/3 left-1/3 w-1/3 h-1/4 border-4 border-gray-400 bg-gray-700">
+        <div class="modal-content w-full h-full font-medium text-white flex flex-col justify-between">
+            <p class="text-2xl m-4">Are you sure you want to delete the task <span id="modal_task_name" class="font-bold"></span>?</p>
+            <div class="flex justify-between m-4 font-bold">
+                <button onclick="close_modal()" class="bg-red-600 w-1/3 h-20 text-2xl rounded hover:bg-red-500">Cancel</button>
+                <button onclick="delete_task()" class="bg-green-700 w-1/3 h-20 text-2xl rounded hover:bg-green-600">Yes</button>
+            </div>
+        </div>
     </div>
 
     <div class="flex justify-center">
