05bcb1d1635d96004c2c356f4f376cdc1b9d44fd
lucas-0331 <lucas6.silva@alunos.ifsuldeminas.edu.br>
2024-01-13 14:28:18 -0300

    feat: client-side inertia installed

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,19 @@ import './edit_form.js';
 import './delete.js';
 import './checkbox.js';
 import './save_new_task.js';
+
+// Inertiajs
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+
+createInertiaApp({
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+        return pages[`./Pages/${name}.vue`]
+    },
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .mount(el)
+    },
+})
