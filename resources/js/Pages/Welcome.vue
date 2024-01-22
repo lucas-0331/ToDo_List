4fe9a59a47272e5ded298f9f6b9099680e436a6f
lucas-0331 <lucas6.silva@alunos.ifsuldeminas.edu.br>
2024-01-21 21:46:20 -0300

    style: alterado os nomes e estrutura

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -30,14 +30,14 @@ defineProps({
                 v-if="$page.props.auth.user"
                 :href="route('dashboard')"
                 class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                >Dashboard</Link
+                >Tasks</Link
             >
 
             <template v-else>
                 <Link
                     :href="route('login')"
                     class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                    >Log in</Link
+                    >Login</Link
                 >
 
                 <Link
