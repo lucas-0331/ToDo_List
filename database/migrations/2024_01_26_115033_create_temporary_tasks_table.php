098b49b9d49b111b760ccfe02f659a55211aa90e
lucas-0331 <lucas6.silva@alunos.ifsuldeminas.edu.br>
2024-02-02 15:35:35 -0300

    feat: criado uma flag no database de tarefas temporárias.

--- database/migrations/2024_01_26_115033_create_temporary_tasks_table.php
+++ database/migrations/2024_01_26_115033_create_temporary_tasks_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('temporary_tasks', function (Blueprint $table) {
             $table->id();
+            $table->boolean('flag')->default(false);
             $table->string('name');
             $table->text('description');
             $table->date('date');
