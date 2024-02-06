ed3f2b7b15ceced21cfa04f98903e89d7c5f7076
lucas-0331 <lucas6.silva@alunos.ifsuldeminas.edu.br>
2024-02-05 23:06:51 -0300

    chore: nova rota para envio de email.

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/dashboard', [TaskController::class, 'dashboard'])
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
-Route::middleware('auth')->name('task.')->group(function () {
+Route::middleware(['auth', 'verified'])->name('task.')->group(function () {
     Route::get('task/create', [TaskController::class, 'create'])->name('create');
     Route::post('task/store', [TaskController::class, 'store'])->name('store');
     Route::get('task/{task}/show', [TaskController::class, 'show'])->name('show');
@@ -33,8 +33,9 @@ Route::middleware('auth')->name('task.')->group(function () {
     Route::delete('task/{task}/destroy', [TaskController::class, 'destroy'])->name('destroy');
     Route::patch('task/{task}/status', [TaskController::class, 'status'])->name('status');
 });
+Route::post('email/send', [\App\Http\Controllers\ContactController::class, 'send'])->name('send');
 
-Route::middleware('auth')->name('temporary.')->group(function () {
+Route::middleware(['auth', 'verified'])->name('temporary.')->group(function () {
     Route::get('temporary/task/index', [TemporaryTaskController::class, 'index'])->name('index');
     Route::post('temporary/task/store', [TemporaryTaskController::class, 'store'])->name('store');
     Route::get('temporary/{temporaryTask}/edit', [TemporaryTaskController::class, 'edit'])->name('edit');
@@ -45,7 +46,7 @@ Route::middleware('auth')->name('temporary.')->group(function () {
     Route::patch('temporary/all_task', [TemporaryTaskController::class, 'all_task'])->name('all_task');
 });
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
